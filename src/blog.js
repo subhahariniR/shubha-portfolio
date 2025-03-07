c7730fad2ffd5e0fdb33ffe3b6e1089d1ec844a3
subhahariniR <jeevikaravi03@gmail.com>
2025-03-07 08:17:14 -0800

    update

--- src/blog.js
+++ src/blog.js
@@ -7,17 +7,32 @@ function Portfolio() {
       <nav className="navbar">
         <div className="navbar-left">
           <a href="#home" className="logo">Shubhaharini</a>
+
+          {/* LinkedIn Button Moved Below Name */}
+          <div className="linkedin-container">
+            <a 
+              href="https://www.linkedin.com/in/YOUR-LINKEDIN-USERNAME" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="linkedin-btn"
+            >
+              Connect on LinkedIn
+            </a>
+          </div>
         </div>
+
         <ul className="navbar-right">
           <li><a href="#home">Home</a></li>
           <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About me</a></li>
+          <li><a href="#case-study">photos</a></li>
         </ul>
       </nav>
 
       {/* Centered Text Section */}
       <div className="one">
-        <h3 className="animate-text">Hey </h3>
-        <h1 className="animate-text">SHUBHAHARINI</h1>
+        <h3 className="animate-text">Hey there I'm </h3>
+        <h1 className="animate-text">Shubhaharini!!</h1>
       </div>
     </div>
   );
